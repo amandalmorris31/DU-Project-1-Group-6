a2f07e1c7842e8074c484069fd1d2c8481893ea6
Kathlene Cory <Kathlene.cory@gmail.com>
2020-05-08 18:23:30 -0600

    added js stylings and modal to HTML

---
+++ js-files/styles.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.modal').modal();
+    $('.modal').modal('open');
+
+    $(function () {
+        $('.modal-close').click(function () {
+            $("h1").fadeIn(1000);
+            $(".container").fadeIn(1000);
+        });
+    });
+});
+
+
+
